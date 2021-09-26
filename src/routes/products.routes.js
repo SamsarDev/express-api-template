c2129363cdfd580bfd89c73666d842820a16d3e0
Samuel Sarmientos <67299065+SamsarDev@users.noreply.github.com>
2021-09-25 22:13:28 -0600

    add / routes

---
+++ src/routes/products.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+import * as productsController from '../controllers/products.controller'
+
+const router = Router()
+
+router.get('/', productsController.getProducts)
+router.post('/', productsController.createProduct)
+router.get('/:productId', productsController.getProductById)
+router.put('/:productId', productsController.updateProductById)
+router.delete('/:productId', productsController.deleteProductById)
+
+export default router;
